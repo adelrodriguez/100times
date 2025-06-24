6d5223836a6d3cdd220372c50fdf479e1ed3abf8
Adel Rodríguez <hello@adelrodriguez.com>
2025-06-24 16:30:51 -0400

    feat: progress grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import ProgressGrid from "~/features/check-in/components/progress-grid"
 import { TypographyH1 } from "~/shared/components/ui/typography"
 
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <TypographyH1>100times</TypographyH1>
+      <div className="flex flex-col items-center justify-center max-w-xl w-full p-4">
+        <ProgressGrid progress={Math.floor(Math.random() * 101)} highlightNext />
+      </div>
     </main>
   )
 }
---
+++ src/features/check-in/components/progress-grid.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import { cn } from "~/shared/utils"
+
+export default function ProgressGrid({
+  progress,
+  onPress,
+  highlightNext = false,
+}: {
+  progress: number
+  onPress?: (index: number) => void
+  highlightNext?: boolean
+}) {
+  return (
+    <div className="grid grid-cols-10 gap-2 w-full h-full">
+      {Array.from({ length: 100 }).map((_, idx) => (
+        <GridCell
+          // biome-ignore lint/suspicious/noArrayIndexKey: we don't really need a unique key here
+          key={idx}
+          index={idx}
+          isCompleted={idx <= progress - 1}
+          isNext={idx === progress && highlightNext}
+          onPress={() => onPress?.(idx)}
+        />
+      ))}
+    </div>
+  )
+}
+
+function GridCell({
+  index,
+  isCompleted,
+  isNext,
+  onPress,
+}: {
+  index: number
+  isCompleted: boolean
+  isNext: boolean
+  onPress?: () => void
+}) {
+  console.log(isNext)
+  return (
+    <button
+      className={cn(
+        "aspect-square flex-1 items-center justify-center flex",
+        (isNext || isCompleted) && "cursor-pointer",
+      )}
+      type="button"
+      disabled={!isCompleted}
+      onClick={onPress}
+    >
+      <div
+        className={cn(
+          "border border-primary rounded-md lg:rounded-lg size-full font-bold text-xs flex items-center justify-center",
+          isCompleted || isNext ? "bg-primary text-primary-foreground" : "bg-white text-muted-foreground",
+          isNext && "animate-pulse",
+        )}
+      >
+        {index + 1}
+      </div>
+    </button>
+  )
+}
